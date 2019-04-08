ed85ec6daa09fcb9131db2682e66d463f82ed16e
Roncax <paolo.roncaglioni@mail.polimi.it>
2019-04-08 13:00:11 +0200

    update

--- replaceFilename.py
+++ replaceFilename.py
@@ -1,19 +1,18 @@
 import os
+
 # Replace a string in the name of a given tree folder with a string you want
-
-print('Enter the Path of the files you want to change: ')
-myPath = input()
-print('Now the character you want to change: ')
-oldChar = input()
-print('for the character: ')
-nextChar = input()
-for dirName, subdirList, fileList in os.walk(myPath):
-    paths = (os.path.join(root, filename)
-        for root, _, filenames in os.walk(myPath)
-        for filename in filenames)
-
-
-    for path in paths:
-        newname = path.replace(oldChar, nextChar)
-        if newname != path:
-            os.rename(path, newname)
+if __name__ == "__main__":
+    print('Enter the Path of the files you want to change: ')
+    myPath = input()
+    print('Now the character you want to change: ')
+    oldChar = input()
+    print('for the character: ')
+    nextChar = input()
+    for dirName, subdirList, fileList in os.walk(myPath):
+        paths = (os.path.join(root, filename)
+            for root, _, filenames in os.walk(myPath)
+            for filename in filenames)
+        for path in paths:
+            newname = path.replace(oldChar, nextChar)
+            if newname != path:
+                os.rename(path, newname)
