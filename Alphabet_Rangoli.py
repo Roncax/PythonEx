c175c3a6d84064d5563aa0f49994fe6d0de781db
Roncax <paolo.roncaglioni@mail.polimi.it>
2019-05-27 23:21:52 +0200

    rangoli

--- Alphabet_Rangoli.py
+++ Alphabet_Rangoli.py
@@ -2,11 +2,10 @@ import string
 
 def print_rangoli(size):
     alphabet = list(string.ascii_lowercase)
-    rangoli_door = []
-    for i in range(0, size-1):
+    for i in range(size-1,0,-1):
         right_string = ['-' + alphabet[s] for s in range(0,i)]
-        left_string = right_string[::-1]
-        porcoddio = left_string + list('-'+alphabet[size-i]) + right_string
+        left_string = right_string[:0:-1]
+        porcoddio = left_string  + right_string
         print(''.join(porcoddio))
 
 if __name__ == '__main__':
